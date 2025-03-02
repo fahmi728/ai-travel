485de86eea3966545fc9795b83678f724af26419
fahmi879 <guefrachi.fahmi@isimg.tn>
2025-03-02 23:33:06 +0100

    making the modal for the AI to be able to communicate with the user and get the toast components

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,35 +1,47 @@
 import React, { useEffect, useState } from "react";
 import MinePlacesAutocomplete from "../searchelemnt/MinePlacesAutocomplete";
 import { Input } from "../components/ui/input";
-import { SelectBudgetList, SelectTravelesList } from "../constants/options";
+import {
+  SelectBudgetList,
+  SelectTravelesList,
+  AI_PROMPT,
+} from "../constants/options";
 import { Button } from "../components/ui/button";
+import { toast } from "sonner";
+import { chatSession } from "../service/AImodal";
 
 function CreateTrip() {
   const [fromDate, setfromDate] = useState([]);
   const [location, setLocation] = useState("");
   const handleInputChange = (name, value) => {
-
-
-
     setfromDate({
       ...fromDate,
       [name]: value,
-    })
+    });
   };
 
-  useEffect(()=>{
-    console.log(fromDate)
-  },[fromDate])
+  useEffect(() => {
+    console.log(fromDate);
+  }, [fromDate]);
 
-  const OngenrateTrip=()=>{
-    if (fromDate?.days<1) {
-      console.log('Please the days grater then 0')
+  const OngenrateTrip = async () => {
+    if (
+      (fromDate?.days < 1 && !fromDate?.infos) ||
+      !fromDate?.budget ||
+      !fromDate?.location
+    ) {
+      toast("Please fill all the fields", "error");
       return;
-      
     }
-    console.log(fromDate)
-
-  }
+    const FINAL_PROMPT = AI_PROMPT.replace("{location}", fromDate.location)
+      .replace("{totalDays}", fromDate.days)
+      .replace("{infos}", fromDate.infos)
+      .replace("{budget}", fromDate.budget)
+      .replace("{totalDays}", fromDate.days);
+    console.log(FINAL_PROMPT);
+    const result = await chatSession.sendMessage(FINAL_PROMPT);
+    console.log(result?.response?.text());
+  };
 
   return (
     <div className="sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10">
@@ -55,8 +67,10 @@ function CreateTrip() {
           <h2 className="text-xl my-3 font-medium">
             How many days are you planning to trip?
           </h2>
-          <Input type="number" placeholder={"Ex.4"} 
-          onChange={(e)=>handleInputChange("days",e.target.value)}
+          <Input
+            type="number"
+            placeholder={"Ex.4"}
+            onChange={(e) => handleInputChange("days", e.target.value)}
           />
         </div>
       </div>
@@ -66,7 +80,7 @@ function CreateTrip() {
           {SelectBudgetList.map((item, index) => (
             <div
               key={index}
-              onClick={()=>handleInputChange('budget',item.title)}
+              onClick={() => handleInputChange("budget", item.title)}
               className={`p-4 border rounded-lg hover:shadow-lg cursor-pointer
                 ${fromDate.budget === item.title ? "bg-blue-100 shadow-lg" : ""}
               `}
@@ -87,9 +101,13 @@ function CreateTrip() {
           {SelectTravelesList.map((item, index) => (
             <div
               key={index}
-              onClick={()=>handleInputChange('infos',item.description)}
+              onClick={() => handleInputChange("infos", item.description)}
               className={`p-4 border rounded-lg hover:shadow-lg cursor-pointer
-                ${fromDate.infos === item.description ? "bg-blue-100 shadow-lg" : ""}
+                ${
+                  fromDate.infos === item.description
+                    ? "bg-blue-100 shadow-lg"
+                    : ""
+                }
               `}
             >
               <h2 className="text-4xl">{item.icon}</h2>
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Header from './components/ui/custom/Header.jsx'
-import CreateTrip from './create-trip/index.jsx'
-
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from "./components/ui/custom/Header.jsx";
+import CreateTrip from "./create-trip/index.jsx";
+import { Toaster } from "sonner";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />
-  },{
-    path: '/create-trip',
-    element:<CreateTrip />
-  }
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/create-trip",
+    element: <CreateTrip />,
+  },
+]);
 
-])
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Header />
+    <Toaster />
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
